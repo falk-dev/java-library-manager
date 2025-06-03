626d7d8b92625df8710520f17a1bb0308db48b7b
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2025-06-02 21:06:37 -0300

    -construção do usuario

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,15 +1,52 @@
 package modelo;
 
 public class Usuario {
+	//garanti que o usuario tenha nome, cpf e email
 	private String nome;
 	private String cpf;
 	private String email;
 	
+	//fiz um construtor para fazer o usuario ser  criado com essas caracteristicas
 	public Usuario(String nome, String cpf, String email) {
-		//this serve para validar e meio que deixa usavel as variaveis. n�o sei se entendi errado e isso, desculpa se eu escrevi algum idiota michelle
 		this.nome = nome;
 		this.cpf = cpf;
 		this.email = email;
 	}
+	// aqui eu vou retorna o get nome, cpf e email para pode ve informacao
+	public String getNome(){
+		return nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	// aqui eu coloquei com a necessidade de modificação
+	// talvez precise disso... to em duvida ainda nessa questão
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	//isso aqui é uma anotação que indica ao compilador que
+	//o método que está sendo declarado é uma sobrescrita de um método 
+	//da classe pai (superclasse).
+	@Override
+	public String toString(){
+		String relatorio_user = "\nNome: "+ this.nome;
+		relatorio_user = "\nCpf: "+ this.cpf;
+		relatorio_user = "\nEmail: "+ this.email;
+		return relatorio_user;
+	}
+	// Nao sei se vai precisa disso aqui mais vi que vc myche colocou no livro entao vou colocar
+	// se puder me explica depois oque isso faz agradeço
 }
-
