4fbb869e545dfd746c571e856bef416d989ea166
RUBENSNETO0001 <rubensnetomartinssuarezneto@gmail.com>
2025-06-03 22:25:06 -0300

    Consertei o user e adicionei o início do main.

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -1,4 +1,5 @@
 package aplicacao;
+import java.util.Scanner;
 
 // import java.util.HashMap;
 
@@ -8,6 +9,36 @@ package aplicacao;
 
 public class Aplicacao{
 	public static void main(String[] args) {
+		menu();
+	}
+	// so para acelerar um pouquinho
+	public static void menu() {
+		Scanner tcl = new Scanner(System.in);
+		int op;
 		
+		do {
+			
+			String menu;
+			menu  = "\n=== BIBLIOTECA ===\n";
+			menu += "Escolha uma opção:\n";
+			//aqui nao sei oque mais vc mychelle vai colocar entao vou deixa sem nada
+			menu += "0 - Sair\n";
+			
+			System.out.println("Digite aqui a opção:");
+			op = tcl.nextInt();
+			
+			switch (op) {
+				case 1:
+					System.out.println("op01");
+					break;
+				case 0:
+					System.out.println("Saindo do sistema!");
+					break;
+				default:
+					System.out.println("Opção inválida. Tente novamente.");
+			}
+		}while(op == 0);
+
+        tcl.close();
 	}
 }
